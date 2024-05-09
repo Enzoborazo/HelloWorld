beaa129955edb1317cb191c9d964e89452bf5e4b
Enzoborazo <161028093+Enzoborazo@users.noreply.github.com>
2024-05-09 17:04:52 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+let nome_usuário= prompt('Qual o seu nome?');
+let elemento = document.querySelector ('#nome_usuario');
+
+while (nomeusuário ==) {
+    nome_usuário = prompt('Qual o seu nome?');
+}
+
+if(nome_usuário == null){
+    elemento.textContent= 'Seja muitro bem vinda';
+}else{
+    elemento.textContent = nome_usuário;
+}
+
+elemento.textContent = nome_usuário;
